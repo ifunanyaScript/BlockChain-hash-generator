6aa2d1ee87b51c10150d068cf368ff941eee10b4
Favour Nwajoko <chocorazzee@gmail.com>
2022-07-15 23:03:28 +0100

    Updated program
    This program runs infinitely until we get a valid hash.

--- infinite_generator.py
+++ infinite_generator.py
@@ -24,13 +24,14 @@ def mining_loop(block_number, transactions, previous_hash, pre_zeros_required):
         else:
             nonce += 1
 
-
+# Program run
 if __name__ == "__main__":
     pre_zeros_required = 5
     transactions = """
     Choco->Razzee->78,
     Ifunanya->Script->456
     """
+    # Remember to calculate time, as you mine.
     start_time = time.time()
     print(f"Mining started!")
     new_block_hash =  mining_loop(7, transactions, "000000067hjg990m1l3sv458m00lkrilkf66nmb0kl9a367klmno341v67uk0m07", pre_zeros_required)
